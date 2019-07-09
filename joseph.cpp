0b321edf04bd768ffaa08fed2a5aed6f39f102ea
Damon <1416902360@qq.com>
2019-07-09 22:08:43 +0800

    wu2

--- joseph.cpp
+++ joseph.cpp
@@ -2,4 +2,18 @@
 #include<stdlib.h>
 using namespace std;
 
-void Create
+void Create(LinkList *list, int num[], int n){
+	LNode *head = (LNode*)malloc(sizeof(struct LNode));
+	head->next = NULL;
+	*list = head;
+	LNode* pre = head;
+	LNode* p;
+	for(int i=0;i<n;i++){
+		p = (LNode*)malloc(sizeof(struct LNode));
+		p->data = num[i];
+		p->next = NULL;
+		pre->next = p;
+		pre = p;
+	}
+	pre->next = head->next;
+}
