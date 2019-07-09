445e89532ee9f0f096c144673054316b94901210
zxdemm <1978881290@qq.com>
2019-07-09 22:07:10 +0800

    add Print mothod

--- joseph.cpp
+++ joseph.cpp
@@ -1,3 +1,20 @@
 #include<stdio.h>
 #include<stdlib.h>
 using namespace std;
+typedef struct LNode{
+int data;
+struct LNode *next;
+}LNode,*LinkList;
+
+void Print(LinkList *list){
+LNode *p,*s;
+p = s = *list;
+p = p->next;
+while(p->next != s->next){
+printf("%d ",p->next);
+p = p->next;
+}
+printf("%d ",p->next);
+p = p->next;
+}
+
